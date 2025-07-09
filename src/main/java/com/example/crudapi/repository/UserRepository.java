fca4f67dfd978b2e48708975d870e20eca35aa75
Dharun Kumar <dharun.logic@gmail.com>
2025-07-10 01:46:25 +0530

    Update UserRepository.java

--- src/main/java/com/example/crudapi/repository/UserRepository.java
+++ src/main/java/com/example/crudapi/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 //JPA AutoCreates CRUD methods
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {  //JpaRepository <Entity, Type of PrimaryKey>
-    Optional<User> findByEmail(String email); // SELECT * FROM users WHERE = ?
+    Optional<User> findByEmail(String email); // SELECT * FROM users WHERE email = ?
 }
